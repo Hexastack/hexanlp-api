3ba07a322958058fc247be7c5e8a92b9e32e449a
Mohamed Marrouchi <marrouchi.mohamed@gmail.com>
2023-04-25 23:01:51 +0100

    feat: initial commit

---
+++ index.js
@@ -0,0 +1,3 @@
+const HexaNlp = require('./src/HexaNlp.js');
+
+module.exports = HexaNlp;
---
+++ test/test.js
@@ -0,0 +1,218 @@
+const expect = require('expect.js');
+const HexaNlp = require('../index.js');
+const trainingData = require('./training_data.json');
+const testData = require('./test_data.json');
+const nock = require('nock');
+
+const endpoint = 'http://localhost:5000';
+const token = 'my123Token';
+const project = 'myProject';
+
+const text = "i'm looking for a place in the north of town";
+
+nock(endpoint)
+  .post('/train')
+  .query({
+    project,
+    token,
+  })
+  .reply((uri, requestBody) => {
+    if ('hexanlp_nlu_data' in requestBody) {
+      return [
+        200,
+        { info: 'Success', model: 'xxxxxxx' },
+        //{ header: 'value' }, // optional headers
+      ];
+    } else {
+      return [500, { error: 100, body: 'Train Error' }];
+    }
+  })
+  .post('/evaluate')
+  .query({
+    project: project,
+    token: token,
+  })
+  .reply((uri, requestBody) => {
+    if ('hexanlp_nlu_data' in requestBody) {
+      return [
+        200,
+        {
+          intent_evaluation: {
+            report: '',
+            predictions: '',
+            precision: 0,
+            f1_score: 0,
+            accuracy: 0,
+          },
+        },
+        //{ header: 'value' }, // optional headers
+      ];
+    } else {
+      return [500, { error: 200, body: 'Evaluate Error' }];
+    }
+  })
+  .post('/parse')
+  .query({
+    project: project,
+    token: token,
+  })
+  .reply((uri, requestBody) => {
+    if (requestBody.q === text && requestBody.project === project) {
+      return [
+        200,
+        {
+          text,
+          project,
+          intent_ranking: [],
+          entities: [],
+          intent: {
+            name: 'restaurant_search',
+            confidence: 0.9,
+            f1_score: 0,
+            accuracy: 0,
+          },
+        },
+      ];
+    } else {
+      return [500, { error: 200, body: 'Evaluate Error' }];
+    }
+  });
+
+describe('HexaNlp', function () {
+  describe('Instantiation', function () {
+    it('Should create an instance of HexaNlp', function () {
+      expect(new HexaNlp(endpoint, project, token)).to.be.a(HexaNlp);
+    });
+    it('Should contain doRequest method', function () {
+      expect(new HexaNlp(endpoint, project, token)).to.have.property('request');
+    });
+    it('Must not hold or show the token', function () {
+      const hexanlp = new HexaNlp(endpoint, project, token);
+      for (var key in hexanlp) {
+        expect(hexanlp[key]).to.not.be.equal(token);
+      }
+    });
+  });
+
+  const hexanlp = new HexaNlp(endpoint, project, token);
+
+  describe('Train', function () {
+    it('Should yield on a json response', function () {
+      return hexanlp
+        .train(trainingData)
+        .then((res) => {
+          expect(res).to.be.an(Object);
+          expect(res).to.have.property('info');
+          expect(res).to.have.property('model');
+        })
+        .catch((e) => {
+          expect(e).to.not.be.ok();
+        });
+    });
+  });
+
+  describe('Evaluate', function () {
+    it('Should yield on a json response', function () {
+      return hexanlp
+        .evaluate(testData)
+        .then((res) => {
+          expect(res).to.be.an(Object);
+          expect(res).to.have.property('intent_evaluation');
+          expect(res.intent_evaluation).to.have.property('report');
+          expect(res.intent_evaluation).to.have.property('predictions');
+          expect(res.intent_evaluation).to.have.property('precision');
+          expect(res.intent_evaluation).to.have.property('f1_score');
+          expect(res.intent_evaluation).to.have.property('accuracy');
+        })
+        .catch((e) => {
+          expect(e).to.not.be.ok();
+        });
+    });
+  });
+
+  describe('Parse', function () {
+    it('Should yield on a json response', function () {
+      return hexanlp
+        .parse(text)
+        .then((res) => {
+          expect(res).to.be.an(Object);
+          expect(res).to.have.property('intent');
+          expect(res.intent).to.have.property('name');
+          expect(res.intent).to.have.property('confidence');
+          expect(res.intent.name).to.be.equal('restaurant_search');
+          expect(res).to.have.property('intent_ranking');
+          expect(res.intent_ranking).to.be.an(Array);
+          expect(res).to.have.property('text');
+          expect(res.text).to.be.equal(text);
+          expect(res).to.have.property('entities');
+          expect(res.entities).to.be.an(Array);
+          expect(res).to.have.property('project');
+          expect(res.project).to.be.equal(project);
+        })
+        .catch((e) => {
+          expect(e).to.not.be.ok();
+        });
+    });
+  });
+
+  // TODO: mock the following api calls
+  // describe('Get Mic Infos', function () {
+  //   it('Should yield on the status', function () {
+  //     return hexanlp
+  //       .get('status')
+  //       .then((res) => {
+  //         expect(res).to.be.an(Object);
+  //         expect(res).to.have.property('available_projects');
+  //       })
+  //       .catch((e) => {
+  //         expect(e).to.not.be.ok();
+  //       });
+  //   });
+  //   it('Should yield on the version', function () {
+  //     return hexanlp
+  //       .get('version')
+  //       .then((res) => {
+  //         expect(res).to.be.an(Object);
+  //         expect(res).to.have.property('version');
+  //         expect(res).to.have.property('minimum_compatible_version');
+  //       })
+  //       .catch((e) => {
+  //         expect(e).to.not.be.ok();
+  //       });
+  //   });
+  //   it('Should yield on the config', function () {
+  //     return hexanlp
+  //       .get('config')
+  //       .then((res) => {
+  //         expect(res).to.be.an(Object);
+  //       })
+  //       .catch((e) => {
+  //         expect(e).to.not.be.ok();
+  //       });
+  //   });
+  //   it('Should throw an error', function () {
+  //     return hexanlp.get('undefined_path').catch((e) => {
+  //       expect(e).to.be.ok();
+  //     });
+  //   });
+  // });
+  // describe('Delete model', function () {
+  //   it('Should throw an error of a non existing model', function () {
+  //     return hexanlp.delete('undefined_model').catch((e) => {
+  //       expect(e).to.be.ok();
+  //     });
+  //   });
+  //   it('Should delete existing model', function () {
+  //     return hexanlp
+  //       .train(trainingData)
+  //       .then((res) => {
+  //         return hexanlp.delete(res.model).then((res) => {
+  //           expect(res).to.be.ok();
+  //         });
+  //       })
+  //       .catch((e) => {
+  //         expect(e).to.not.be.ok();
+  //       });
+  //   });
+  // });
+});
